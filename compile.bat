df4b4c59a608a6d9712d2d221d733756914e4a2c
jeppeter <jeppeter@gmail.com>
2026-03-09 16:33:39 +0800

    to add GOPROXY

--- compile.bat
+++ compile.bat
@@ -2,4 +2,5 @@ rem echo off
 
 set TOPBUILD=%~dp0
 echo %TOPBUILD%
+set GOPROXY=https://goproxy.cn
 pushd %CD% && cd %TOPBUILD%\build\ &&  go build -o build.exe ci.go && cd %TOPBUILD% && .\build\build.exe install .\cmd\geth
