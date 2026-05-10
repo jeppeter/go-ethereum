39b7f7b66d3c5024c80adb1b0ab928cff58eb273
jeppeter <jeppeter@gmail.com>
2026-05-10 19:22:30 +0800

    to make compile.bat

--- compile.bat
+++ compile.bat
@@ -3,4 +3,5 @@ rem echo off
 set TOPBUILD=%~dp0
 echo %TOPBUILD%
 set GOPROXY=https://goproxy.cn
+set GO111MODULE=auto
 pushd %CD% && cd %TOPBUILD%\build\ &&  go build -o build.exe ci.go && cd %TOPBUILD% && .\build\build.exe install .\cmd\geth
