09edb66aac8be0f91164ff094b8b7a1e09a9e877
jeppeter <jeppeter@gmail.com>
2026-06-04 13:29:42 +0800

    to compile devp2p

--- compile.bat
+++ compile.bat
@@ -4,4 +4,4 @@ set TOPBUILD=%~dp0
 echo %TOPBUILD%
 set GOPROXY=https://goproxy.cn
 set GO111MODULE=auto
-pushd %CD% && cd %TOPBUILD%\build\ &&  go build -o build.exe ci.go && cd %TOPBUILD% && .\build\build.exe install .\cmd\geth && .\build\build.exe install .\cmd\clef && popd || popd
+pushd %CD% && cd %TOPBUILD%\build\ &&  go build -o build.exe ci.go && cd %TOPBUILD% && .\build\build.exe install .\cmd\geth && .\build\build.exe install .\cmd\clef && .\build\build.exe install .\cmd\devp2p && popd || popd
